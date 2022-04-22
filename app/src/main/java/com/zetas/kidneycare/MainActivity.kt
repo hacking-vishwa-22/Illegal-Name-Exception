9793f76cf143059bc61d052a2fb8b626c2810d9a
Joaquin144 <vvpp898.comco@gmail.com>
2022-04-22 18:57:24 +0530

    initialised project

---
+++ app/src/main/java/com/zetas/kidneycare/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.zetas.kidneycare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Kindey care
+        //todo :
+    }
+}
